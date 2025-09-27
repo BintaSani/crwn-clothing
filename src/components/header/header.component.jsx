b49a9dd699b678811363fbc705098460524d849e
BintaSani <bintasani992@gmail.com>
2025-09-27 16:31:53 +0100

    removed admin

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,77 +1,63 @@
-import React from 'react';
+import React from "react";
 
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 //import { auth } from '../../firebase/firebase.utils';
-import CartIcon from '../cart-icon/cart-icon.component';
-import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-import { selectCartHidden } from '../../redux/cart/cart.selectors';
-import { selectCurrentUser } from '../../redux/user/user.selector';
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selector";
 
-import {ReactComponent as Logo} from '../../assets/4.1 crown.svg';
-import { HeaderContainer,LogoContainer,OptionsContainer,OptionLink } from './header.styles';
-import { signOutStart } from '../../redux/user/user.actions';
-import { selectManagementHidden } from '../../redux/management/management.selector';
-import { auth } from '../../firebase/firebase.utils';
-import Drop from '../dp/dp.component';
-
+import { ReactComponent as Logo } from "../../assets/4.1 crown.svg";
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink,
+} from "./header.styles";
+import { signOutStart } from "../../redux/user/user.actions";
+import { selectManagementHidden } from "../../redux/management/management.selector";
+import { auth } from "../../firebase/firebase.utils";
+import Drop from "../dp/dp.component";
 
 const Header = ({ currentUser, hide, hidden, signOutStart }) => {
-  const user =auth.currentUser;
-  return(
+  const user = auth.currentUser;
+  return (
     <HeaderContainer>
-      <LogoContainer to='/'>
-        <Logo className='logo'/>
+      <LogoContainer to="/">
+        <Logo className="logo" />
       </LogoContainer>
       <OptionsContainer>
-        <OptionLink to='/shop'>
-          SHOP
-        </OptionLink>
-      
-        <OptionLink to='/shop'>
-          CONTACT
-        </OptionLink>
-      
-        {
-          hide ? null
-          :
-          <OptionLink to='/admin'>
-            ADMIN
+        <OptionLink to="/shop">SHOP</OptionLink>
+
+        <OptionLink to="/shop">CONTACT</OptionLink>
+
+        {hide ? null : <OptionLink to="/admin">ADMIN</OptionLink>}
+
+        {currentUser ? (
+          <OptionLink as="div" onClick={signOutStart}>
+            SIGN OUT
           </OptionLink>
-        }
-        
-
-        {
-          currentUser ? (
-          <OptionLink as='div' onClick={signOutStart}>SIGN OUT</OptionLink>) 
-          :
-          (<OptionLink to='/signin'>SIGN IN</OptionLink>
-          )}
-        <CartIcon/>
-        {
-        hide ? <Drop src={user ? user.photoURL : ''} alt='' /> : null
-        }
+        ) : (
+          <OptionLink to="/signin">SIGN IN</OptionLink>
+        )}
+        <CartIcon />
+        {hide ? <Drop src={user ? user.photoURL : ""} alt="" /> : null}
       </OptionsContainer>
-      {
-        hidden ? null : <CartDropdown/>
-      }
-      
-      
-    </HeaderContainer>)
+      {hidden ? null : <CartDropdown />}
+    </HeaderContainer>
+  );
 };
-
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
   hide: selectManagementHidden,
-  hidden: selectCartHidden
+  hidden: selectCartHidden,
 });
 
-const mapDispatchToProps = dispatch => ({
-  signOutStart: () => dispatch(signOutStart())
-})
+const mapDispatchToProps = (dispatch) => ({
+  signOutStart: () => dispatch(signOutStart()),
+});
 
-
-
-export default connect(mapStateToProps,mapDispatchToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,74 +1,89 @@
-import React from 'react';
-import { useState } from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { useState } from "react";
+import { connect } from "react-redux";
 
-import FormInput from '../form-input/form-input.component';
-import CustomButton from '../custom-button/custom-button.component';
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
-import { managementHidden } from '../../redux/management/management.actions';
-import { googleSignInStart , emailSignInStart } from '../../redux/user/user.actions';
-import { SignInContainer, SignInTitle, ButtonsBarContainer, CheckBox, Label } from './sign-in.styles';
+// import { managementHidden } from "../../redux/management/management.actions";
+import {
+  googleSignInStart,
+  emailSignInStart,
+} from "../../redux/user/user.actions";
+import {
+  SignInContainer,
+  SignInTitle,
+  ButtonsBarContainer,
+  CheckBox,
+  Label,
+} from "./sign-in.styles";
 
+const SignIn = ({ emailSignInStart, googleSignInStart }) => {
+  const [userCredentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
 
+  const { email, password } = userCredentials;
 
-const SignIn = ({emailSignInStart, googleSignInStart, managementHidden}) => {
-    const [userCredentials, setCredentials] = useState({email: '', password: ''})
-   
-    const {email, password} = userCredentials;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async e => {
-        e.preventDefault();
-       
-        emailSignInStart(email,password);
+    emailSignInStart(email, password);
+  };
 
-    };
+  const handleChange = (e) => {
+    const { value, name } = e.target;
 
-    const handleChange = e => {
-        const { value, name } = e.target;
+    setCredentials({ ...userCredentials, [name]: value });
+  };
 
-        setCredentials({...userCredentials, [name]: value});
-    };
+  return (
+    <SignInContainer>
+      <SignInTitle>I already have an account</SignInTitle>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          name="email"
+          type="email"
+          value={email}
+          handleChange={handleChange}
+          label="email"
+          required
+        />
 
-    return(
-        <SignInContainer>
-            <SignInTitle>I already have an account</SignInTitle>
-            <span>Sign in with your email and password</span>
-            <form onSubmit={handleSubmit}>
-                <FormInput 
-                name='email' 
-                type='email' 
-                value={email} 
-                handleChange={handleChange}
-                label='email'
-                required/>  
-                
-                <FormInput 
-                name='password' 
-                type='password' 
-                value={password} 
-                handleChange={handleChange}
-                label='password'
-                required/>  
+        <FormInput
+          name="password"
+          type="password"
+          value={password}
+          handleChange={handleChange}
+          label="password"
+          required
+        />
 
-                <CheckBox type='checkbox' id='vendor' onClick={managementHidden}/><Label >Admin</Label>
+        {/* <CheckBox type="checkbox" id="vendor" onClick={managementHidden} />
+        <Label>Admin</Label> */}
 
-                <ButtonsBarContainer>
-                <CustomButton type='submit' >Sign In</CustomButton>
-                <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn>Sign in with Google </CustomButton>
-                </ButtonsBarContainer>
-            </form>
-        </SignInContainer>
-    );
-    
-}
+        <ButtonsBarContainer>
+          <CustomButton type="submit">Sign In</CustomButton>
+          <CustomButton
+            type="button"
+            onClick={googleSignInStart}
+            isGoogleSignIn
+          >
+            Sign in with Google{" "}
+          </CustomButton>
+        </ButtonsBarContainer>
+      </form>
+    </SignInContainer>
+  );
+};
 
-const mapDispatchToProps = dispatch =>({
-    managementHidden: () => dispatch(managementHidden()),
-    googleSignInStart: () => dispatch(googleSignInStart()) ,
-    emailSignInStart: (email, password) => 
-      dispatch(emailSignInStart({email, password}))
-    
+const mapDispatchToProps = (dispatch) => ({
+  //   managementHidden: () => dispatch(managementHidden()),
+  googleSignInStart: () => dispatch(googleSignInStart()),
+  emailSignInStart: (email, password) =>
+    dispatch(emailSignInStart({ email, password })),
 });
 
-
-export default connect(null,mapDispatchToProps)(SignIn);
+export default connect(null, mapDispatchToProps)(SignIn);
